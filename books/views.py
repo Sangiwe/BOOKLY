dcc19bb7e4538639a41e6e96203ee74145d961f8
Sangiwe <nokubongasangiwe@gmail.com>
2025-12-14 17:19:36 +0200

    Add reading status endpoints for books

--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -18,3 +21,43 @@ class BookViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def start_reading(self, request, pk=None):
+        book = self.get_object()
+        book.status = Book.READING
+        book.save()
+        return Response(
+            {"message": "Book marked as currently reading"},
+            status=status.HTTP_200_OK
+        )
+
+    @action(detail=True, methods=['post'])
+    def finish(self, request, pk=None):
+        book = self.get_object()
+        book.status = Book.READ
+        book.save()
+        return Response(
+            {"message": "Book marked as read"},
+            status=status.HTTP_200_OK
+        )
+
+    @action(detail=True, methods=['post'])
+    def want_to_read(self, request, pk=None):
+        book = self.get_object()
+        book.status = Book.WANT_TO_READ
+        book.save()
+        return Response(
+            {"message": "Book marked as want to read"},
+            status=status.HTTP_200_OK
+        )
+
+    @action(detail=True, methods=['post'])
+    def drop(self, request, pk=None):
+        book = self.get_object()
+        book.status = Book.DROPPED
+        book.save()
+        return Response(
+            {"message": "Book marked as dropped"},
+            status=status.HTTP_200_OK
+        )
